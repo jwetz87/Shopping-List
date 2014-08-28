805c140a5e0710e9d69c2bbe3777f2f9d3d21c8f
jwetz87 <j.wetz87@gmail.com>
2014-08-27 20:00:07 -0500

    more changes

--- js/app.js
+++ js/app.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
 	
-	$(".additem").on("click", function() {
-		var $item_title = $(this).closest(".buttoncontainer").find(".iteminput");
-        $( ".grocery_item" ).each().val();
-          if (.grocery_item == "") {
-              $item_title
+	$(".additem").on("click", function () {
+		var $item_title = $(".iteminput").text($(this).val());
+       alert($item_title); });
+    
+    
+        
+        //$( ".grocery_item" ).each().val();
+          //if (.grocery_item == "") {
+            //  $item_title
               
 			
 	
