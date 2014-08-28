062814782a14bee3c9bf33f524250886dcb21fb9
jwetz87 <j.wetz87@gmail.com>
2014-08-27 19:17:11 -0500

    code changes again
    code changes

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
 	
-	
-	
 	$(".additem").on("click", function() {
 		var $item_title = $(this).closest(".buttoncontainer").find(".iteminput");
+        $( ".grocery_item" ).each().val();
+          if (.grocery_item == "") {
+              $item_title
+              
 			
 	
 	
 	
 	
 });
+        
+    
