738f9e3c03af09ff1d3ad00fbf65410a039f19b1
jwetz87 <j.wetz87@gmail.com>
2014-08-30 19:15:53 -0500

    Code firing but on all items

--- js/app.js
+++ js/app.js
@@ -2,18 +2,12 @@ $(document).ready(function () {
 	
 	$(".additem").on("click", function () {
 		var item_title = $(".iteminput").val();
-       alert(item_title); });
-	
-	$( "li" ).each(function( index ) {
-		 (index + "; " + $( this ).text());
-	
-
-	
-   
-	
-	
-});
+	   $( "li" ).each(function( index ) {
+		 (index + ".empty ".closest + $( this ).text(item_title));
+       });
         
+	});
+});        
     
 
 
