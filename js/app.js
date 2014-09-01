a766d8e785a2d8d0f9348d8903ab41a2e1a7dc98
jwetz87 <j.wetz87@gmail.com>
2014-09-01 10:18:27 -0500

    consolelogs

--- js/app.js
+++ js/app.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
 	$(".additem").on("click", function () {
 		var item_title = $(".iteminput").val();
 	   $( "li" ).each(function( index ) {
+		   console.log("index");
+		   console.log(index);
+		   console.log(this);
+		   return false;
 		 (index + ".empty_list_item " + ".grocery_item" + $( this ).text(item_title));
        });
         
