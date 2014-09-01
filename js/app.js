056baaeb2d69627f073432e96a7d1d3bd99c2dd1
jwetz87 <j.wetz87@gmail.com>
2014-09-01 12:15:56 -0500

    Code working better

--- js/app.js
+++ js/app.js
@@ -1,15 +1,51 @@
 $(document).ready(function () {
 	
-	$(".additem").on("click", function () {
+	$(".additem").on("click", function (event) {
 		var item_title = $(".iteminput").val();
-	   $( "li" ).each(function( index ) {
-		   console.log("index");
-		   console.log(index);
-		   console.log(this);
+	   $( "li.list_item" ).each(function( index ) {
+		   if($(this).hasClass("empty")){
+		   $(this).find(".grocery_item").text($.trim(item_title));
+		   $(this).toggleClass("empty");
+		   
+		 
 		   return false;
-		 (index + ".empty_list_item " + ".grocery_item" + $( this ).text(item_title));
+
+		   }
+		   
        });
         
+	});
+	
+	$(".notepad").on("click", function (event) {
+	
+		var t = event.target;
+		// If it is a grocery item add class 'aquired' to parent list item
+		// Don't forget to add css for grocery items and remove button
+		// consider toggling aquired in case of missclick
+		// Else If it is the remove button that was clicked
+		
+		// 1 Locate sibling grocery item and set text to empty string
+		// 2 toggle class aquired to parent list item
+		// 3 toggle class empty to parent list item
+		if(){
+		
+		
+		} else if(){
+		
+		
+		
+		
+		} else {
+		
+		console.log("You Are Doing It Wrong")
+		
+		}
+	
+	
+	
+	
+	
+	
 	});
 	
 		//$(".clearlist").on("click", function() {
@@ -27,3 +63,10 @@ $(document).ready(function () {
 	//.hasClass
 	//.toggleClass
 	
+
+
+		   //console.log("index");
+		   //console.log(index);
+		   //console.log(this);
+		   //console.log ($(this).find(".grocery_item"));
+		   //console.log($(this).hasClass("empty"));
