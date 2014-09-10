8b98c1680eaa66f4b582af3091b998fcea429e6d
jwetz87 <j.wetz87@gmail.com>
2014-09-09 19:41:34 -0500

    enter key working
    enter working

--- js/app.js
+++ js/app.js
@@ -15,10 +15,40 @@ $(document).ready(function () {
        });
         
 	});
+    
+    $(".iteminput").keypress(function ( event ) {
+       if (event.which ==13 ) {
+           var item_title = $(".iteminput").val();
+	   $( "li.list_item" ).each(function( index ) {
+		   if($(this).hasClass("empty")){
+		   $(this).find(".grocery_item").text($.trim(item_title));
+		   $(this).toggleClass("empty");
+		   
+		 
+		   return false;
+
+		   }
+		   
+       });
+           
+           
+       }
+        
+        
+        
+    });
+   
+   // $('.additem').on("click" , function(event){
+     //       if($('iteminput').val().length > 0) {
+       //         $(this).val('');
+         //   }
+    //}
+    
+});
 	
-	$(".notepad").on("click", function (event) {
+	//$(".notepad").on("click", function (event) {
 	
-		var t = event.target;
+	//	var t = event.target;
 		// If it is a grocery item add class 'aquired' to parent list item
 		// Don't forget to add css for grocery items and remove button
 		// consider toggling aquired in case of missclick
@@ -27,32 +57,31 @@ $(document).ready(function () {
 		// 1 Locate sibling grocery item and set text to empty string
 		// 2 toggle class aquired to parent list item
 		// 3 toggle class empty to parent list item
-		if(){
+	//	if(){
 		
 		
-		} else if(){
+	//	} else if(){
 		
 		
 		
 		
-		} else {
+	//	} else {
 		
-		console.log("You Are Doing It Wrong")
+	//	console.log("You Are Doing It Wrong")
 		
-		}
+	//	}
 	
 	
 	
 	
 	
 	
-	});
+	// });
 	
 		//$(".clearlist").on("click", function() {
 		//$( "li" ).each(function( index ) {
 		//	(index + ".
 	
-});        
     
 
 
